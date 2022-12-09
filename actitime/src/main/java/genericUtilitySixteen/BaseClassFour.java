9ee94546d3495d4f4f3aa20c2e6871d674f29bc1
smitk <smitk@DESKTOP-IAGOEDI>
2022-12-09 14:25:54 +0530

    Commit2

--- actitime/src/main/java/genericUtilitySixteen/BaseClassFour.java
+++ actitime/src/main/java/genericUtilitySixteen/BaseClassFour.java
@@ -1,7 +1,7 @@
 package genericUtilitySixteen;
 
 public class BaseClassFour {
-
+//my change for commit2
 }
 //write prog then add tstNG library or add by rt.click on maven project
 //then go to buildpath then add libray
